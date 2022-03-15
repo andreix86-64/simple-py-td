8434330dcc05bd8c12111c343cd80d19ca152f32
ramos <ramoscu980@gmail.com>
2022-03-15 16:04:54 +0100

    added buttons.py

---
+++ ui/widgets/buttons.py
@@ -0,0 +1,8 @@
+import pygame
+import pygame.freetype
+import lib.constants as c
+from generics.generics import RenderableObject
+
+class SimpleButton(RenderableObject):
+    def __init__(self, text,x=0,y=0):
+        pass
