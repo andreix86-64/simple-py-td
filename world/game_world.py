ded77fd35667d0d1e57b98c3805552a2d203b01d
Andrei Florea <andrei.florea@student.uni-augsburg.de>
2022-03-16 15:12:25 +0100

    added path debuging

--- world/game_world.py
+++ world/game_world.py
@@ -8,6 +8,8 @@ from world.path_finder import PathFinder
 class GameWorld:
     def __init__(self, engine):
         self.engine = engine
+        self.screen = self.engine.screen
+
         self.size = 15
         self.cell_size = 20
 
@@ -34,11 +36,25 @@ class GameWorld:
 
         # Grid path of the enemy
         self.enemy_grid_path = self.path_finder.get_single_path(self.start[0], self.finish[0])
+        self.enemy_screen_path = self.compute_screen_path()
 
     def cell_color(self, i, j):
         if self.grid[i][j] == 1:
             return 10, 10, 10
         return c.BACKGROUND_COLOR
+
+    def compute_screen_path(self):
+        path = []
+        for i in range(len(self.enemy_grid_path)):
+            a = self.enemy_grid_path[i]
+
+            x1 = a[1] * self.cell_size + self.cell_size / 2
+            y1 = a[0] * self.cell_size + self.cell_size / 2
+
+            path.append((x1, y1))
+
+        self.enemy_screen_path = path
+        return path
 
     def update(self):
         """ Updates the world parameters based on the engine """
@@ -46,13 +62,17 @@ class GameWorld:
         self.cell_size = math.ceil(self.engine.window_width / self.size)
 
     def render_enemy_path(self):
-        pass
+        for i in range(len(self.compute_screen_path()) - 1):
+            a = self.enemy_screen_path[i]
+            b = self.enemy_screen_path[i + 1]
+
+            pygame.draw.line(self.screen, c.RED, a, b, 3)
 
     def render_grid(self):
         """ Renders the grid """
         for x in range(self.size):
             for y in range(self.size):
-                pygame.draw.rect(self.engine.screen, self.cell_color(x, y),
+                pygame.draw.rect(self.screen, self.cell_color(x, y),
                                  pygame.Rect(y * self.cell_size, x * self.cell_size, self.cell_size, self.cell_size))
 
         # Debugging:
